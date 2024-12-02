1adb6a831d811b080376830f29853c0b4585f7f3
Javier Parada <javierparada@pm.me>
2024-12-02 16:20:28 +0100

    sdd deleted event toop

---
+++ libs/cti/src/cves/domain/events/cve_deleted_event.rs
@@ -0,0 +1,57 @@
+
+use events::domain::domain_event::DomainEvent;
+
+use crate::cves::domain::entities::{cve_description::CveDescription, cve_id::CveId, cve_publication_date::CvePublicationDate, cve_state::CveState};
+
+
+pub struct CveDeletedEvent {
+    pub id: String,
+    
+    pub cve_id: CveId,
+    pub cve_state: CveState,
+    pub cve_date_published: CvePublicationDate,
+    pub cve_description: CveDescription,
+
+    pub occurred_on: String,
+}
+
+impl CveDeletedEvent {
+    pub fn new(
+        cve_id: CveId, 
+        cve_state: CveState, 
+        cve_date_published: CvePublicationDate, 
+        cve_description: CveDescription,
+    ) -> Self {
+        let id = uuid::Uuid::new_v4().to_string();
+        let occurred_on = chrono::Utc::now().to_rfc3339();
+        Self { id, cve_id, cve_state, cve_date_published, cve_description, occurred_on }
+    }
+
+    pub fn new_shared(
+        cve_id: CveId, 
+        cve_state: CveState, 
+        cve_date_published: CvePublicationDate, 
+        cve_description: CveDescription,
+    ) -> std::sync::Arc<Self> {
+        std::sync::Arc::new(Self::new(cve_id, cve_state, cve_date_published, cve_description))
+    }
+}
+
+impl DomainEvent for CveDeletedEvent {
+    fn event_type(&self) -> String {
+        "com.tanukibox.cti.cve.deleted@1.0.0".to_string()
+    }
+}
+
+impl Clone for CveDeletedEvent {
+    fn clone(&self) -> Self {
+        let mut event = Self::new(
+            self.cve_id.clone(),
+            self.cve_state.clone(),
+            self.cve_date_published.clone(),
+            self.cve_description.clone(),
+        );
+        event.occurred_on = self.occurred_on.clone();
+        return event;
+    }
+}
--- libs/cti/src/cves/domain/events/mod.rs
+++ libs/cti/src/cves/domain/events/mod.rs
@@ -1,2 +1,3 @@
 pub mod cve_created_event;
 pub mod cve_updated_event;
+pub mod cve_deleted_event;
