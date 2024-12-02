08ea76040e88e55de4de336f3e32f06be97caea5
Javier Parada <javierparada@pm.me>
2024-12-02 12:41:38 +0100

    add cve impl

--- libs/cti/src/cves/domain/entities/cve.rs
+++ libs/cti/src/cves/domain/entities/cve.rs
@@ -1,3 +1,5 @@
+use aggregate_root::domain::aggregate_root::AggregateRoot;
+
 use super::{cve_description::CveDescription, cve_id::CveId, cve_publication_date::CvePublicationDate, cve_state::CveState};
 
 
@@ -7,4 +9,37 @@ pub struct Cve {
     pub date_published: CvePublicationDate,
     
     pub description: CveDescription,
+}
+
+impl Cve {
+    pub fn new(
+        id: CveId, 
+        state: CveState, 
+        date_published: CvePublicationDate, 
+        description: CveDescription
+    ) -> Self {
+        Self {
+            id,
+            state,
+            date_published,
+            description,
+        }
+    }
+}
+
+impl AggregateRoot for Cve {
+    fn get_type() -> String {
+        "com.tanukibox.tanuki-library.cti.cve".to_string()
+    }
+}
+
+impl Clone for Cve {
+    fn clone(&self) -> Self {
+        Self::new(
+            self.id.clone(),
+            self.state.clone(),
+            self.date_published.clone(),
+            self.description.clone(),
+        )
+    }
 }
