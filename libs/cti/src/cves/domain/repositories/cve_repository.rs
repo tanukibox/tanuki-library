b362b4cc87e68433f385a46f48abbb1ff6be97d9
Javier Parada <javierparada@pm.me>
2024-12-03 10:11:59 +0100

    add cve repo

---
+++ libs/cti/src/cves/domain/repositories/cve_repository.rs
@@ -0,0 +1,10 @@
+use crate::{cves::domain::entities::{cve::Cve, cve_id::CveId}, shared::domain::errors::DomainError};
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait CveRepository: Send + Sync + 'static {
+    async fn find_by_id(&self, id: &CveId) -> Result<Cve, DomainError>;
+    async fn create_one(&self, user: &Cve) -> Result<(), DomainError>;
+    async fn update_one(&self, user: &Cve) -> Result<(), DomainError>;
+    async fn delete_one(&self, id: &CveId) -> Result<(), DomainError>;
+}
