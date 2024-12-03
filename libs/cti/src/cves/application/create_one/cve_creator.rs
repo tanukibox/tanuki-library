536d592c59e95a1243558c6f5cf0a9374181efaf
Javier Parada <javierparada@pm.me>
2024-12-03 16:23:58 +0100

    update tracing

--- libs/cti/src/cves/application/create_one/cve_creator.rs
+++ libs/cti/src/cves/application/create_one/cve_creator.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use events::domain::event_bus::EventBus;
-use tracing::debug;
 
 use crate::{cves::domain::{entities::{cve::Cve, cve_description::CveDescription, cve_id::CveId, cve_publication_date::CvePublicationDate, cve_state::CveState}, events::cve_created_event::CveCreatedEvent, repositories::cve_repository::CveRepository}, shared::domain::errors::DomainError};
 
@@ -23,16 +22,16 @@ impl<R: CveRepository, E: EventBus> CveCreator<R, E> {
         date_published: CvePublicationDate,
         description: CveDescription,
     ) -> Result<(), DomainError> {
-        debug!("Starting CVE creation for {}.", id);
+        tracing::debug!("Starting CVE creation for {}.", id);
         let key = Cve::new(&id, &state, &date_published, &description);
         let res = self.repository.create_one(&key).await;
         if res.is_err() {
-            debug!("Error creating CVE with id: {}.", id);
+            tracing::info!("Error creating CVE with id: {}.", id);
             return Err(res.err().unwrap());
         }
         let created_event = CveCreatedEvent::new_shared(&id, &state, &date_published, &description);
         self.event_bus.publish(created_event).await;
-        debug!("CVE with id: {} created.", id);
+        tracing::debug!("CVE with id: {} created.", id);
         Ok(())
     }
 }
