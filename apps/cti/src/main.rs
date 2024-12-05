5158d30c1f2f4b2ad4ef4df60a04010e3d1f97fa
Javier Parada <javierparada@pm.me>
2024-12-05 11:17:24 +0100

    create main script

--- apps/cti/src/main.rs
+++ apps/cti/src/main.rs
@@ -1,7 +1,71 @@
 pub mod v1;
 
+use std::sync::{atomic::{AtomicU16, Ordering}, Arc};
+
+use actix_web::{web::Data, App, HttpServer};
+use cqrs::{domain::query_bus::QueryBus, infrastructure::inmemory::inmemory_query_bus::InMemoryQueryBus};
+use cti::cves::{application::{create_one::cve_creator::CveCreator, delete_one::cve_deleter::CveDeleter, find_one::{cve_finder::CveFinder, find_cve_q_handler::FindCveQueryHandler}}, infrastructure::sqlx::sqlx_postgres_cve_repository::SqlxPostgresCveRepository};
+use events::infrastructure::inmemory::inmemory_event_bus::InMemoryEventBus;
+use tracing::{self as logger};
+use v1::health::health_controller;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::from_path("./apps/cti/.env").ok();
-    Ok(())
+
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        //.pretty()
+        .with_timer(tracing_subscriber::fmt::time::ChronoUtc::rfc3339())
+        .init();
+
+    let host = "0.0.0.0";
+    let port = std::env::var("PORT").expect("PORT must be set");
+    let address = format!("{}:{}", host, port);
+    logger::info!("Starting server at http://{}:{}/.", host, port);
+
+    let thread_counter = Arc::new(AtomicU16::new(0));
+
+    let event_bus = InMemoryEventBus::new();
+    let event_bus_ref = Arc::new(event_bus);
+
+    let mut query_bus = InMemoryQueryBus::new();
+
+    let cve_repository = SqlxPostgresCveRepository::from_env().await;
+    let cve_repository_ref = Arc::new(cve_repository);
+
+    let cve_finder = CveFinder::new(cve_repository_ref.clone());
+    let find_cve_q_handler = FindCveQueryHandler::new(cve_finder);
+    let find_cve_q_handler_ref = Arc::new(find_cve_q_handler);
+    query_bus.register(find_cve_q_handler_ref);
+
+    let cve_creator = CveCreator::new(cve_repository_ref.clone(), event_bus_ref.clone());
+    let cve_creator_ref = Data::new(cve_creator);
+
+    let cve_deleter = CveDeleter::new(cve_repository_ref.clone(), event_bus_ref.clone());
+    let cve_deleter_ref = Data::new(cve_deleter);
+
+    let query_bus_ref: Data<Arc<dyn QueryBus>> = Data::new(Arc::new(query_bus));
+    
+    HttpServer::new(move || {
+        let thread_counter = thread_counter.fetch_add(1, Ordering::SeqCst);
+        logger::info!("Thread {} started.", thread_counter);
+
+        let cors = actix_cors::Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_header()
+            .max_age(3600);
+
+        App::new()
+            .wrap(cors)
+            .app_data(query_bus_ref.clone())
+            .configure(health_controller::router)
+    })
+    .bind(&address)
+    .unwrap_or_else(|err| {
+        panic!("🔥🔥🔥 Can not bind to address: {} 🔥🔥🔥", err);
+    })
+    .run()
+    .await
 }
