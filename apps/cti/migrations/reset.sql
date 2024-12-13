e1af775e58bbe4aa78f96091fbdf63f350edeae9
Javier Parada <javierparada@pm.me>
2024-12-13 11:23:06 +0100

    update db

--- apps/cti/migrations/reset.sql
+++ apps/cti/migrations/reset.sql
@@ -4,8 +4,11 @@ DROP TABLE IF EXISTS cti.cves;
 CREATE TABLE IF NOT EXISTS cti.cves (
     id text PRIMARY KEY,
     state text NOT NULL,
-	date_published text NOT null,
 	description text NOT NULL,
+    assigner_id text NOT NULL,
+    assigner_name text NOT NULL,
+    date_published text NOT null,
+    date_updated text NOT null,
 
     _created_at timestamp with time zone DEFAULT now()
 );
