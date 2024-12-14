76df20406d6bc0e0363ba1fa723d2b04a7dbbb7f
Javier Parada <javierparada@pm.me>
2024-12-14 11:12:58 +0100

    add breach repository

---
+++ libs/cti/src/breaches/domain/repositories/breach_repository.rs
@@ -0,0 +1,12 @@
+use crate::{
+    breaches::domain::entities::{breach::Breach, breach_id::BreachId}, shared::domain::errors::DomainError
+};
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait BreachRepository: Send + Sync + 'static {
+    async fn find_by_id(&self, id: &BreachId) -> Result<Breach, DomainError>;
+    async fn create_one(&self, breach: &Breach) -> Result<(), DomainError>;
+    async fn update_one(&self, breach: &Breach) -> Result<(), DomainError>;
+    async fn delete_one(&self, id: &BreachId) -> Result<(), DomainError>;
+}
---
+++ libs/cti/src/breaches/domain/repositories/mod.rs
@@ -0,0 +1,1 @@
+pub mod breach_repository;
