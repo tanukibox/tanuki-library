0143d4d51c50cf22fef8ba19319b70151c2aa16f
Javier Parada <javierparada@pm.me>
2024-12-15 10:53:42 +0100

    add find one breach query

---
+++ libs/cti/src/breaches/application/find_one/find_breach_query.rs
@@ -0,0 +1,32 @@
+use std::any::Any;
+
+use cqrs::domain::query::Query;
+
+pub struct FindBreachQuery {
+    pub cve_id: Option<String>,
+    pub vendor: Option<String>,
+    pub product: Option<String>,
+    pub version: Option<String>,
+}
+
+impl FindBreachQuery {
+    pub const QUERY_TYPE: &'static str = "FindBreachQuery";
+
+    pub fn new(cve_id: Option<String>, vendor: Option<String>, product: Option<String>, version: Option<String>) -> FindBreachQuery {
+        FindBreachQuery { cve_id, vendor, product, version }
+    }
+
+    pub fn new_boxed(cve_id: Option<String>, vendor: Option<String>, product: Option<String>, version: Option<String>) -> Box<dyn Query> {
+        Box::new(FindBreachQuery::new(cve_id, vendor, product, version))
+    }
+}
+
+impl Query for FindBreachQuery {
+    fn get_type(&self) -> String {
+        FindBreachQuery::QUERY_TYPE.to_string()
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
