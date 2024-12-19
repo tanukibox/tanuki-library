725d07687752f068bab30541f475e318098dbf14
Javier Parada <javierparada@pm.me>
2024-12-19 12:32:44 +0100

    complete repository

--- libs/cti/src/breaches/infrastructure/sqlx/sqlx_postgres_breach_repository.rs
+++ libs/cti/src/breaches/infrastructure/sqlx/sqlx_postgres_breach_repository.rs
@@ -44,12 +44,21 @@ impl BreachRepository for SqlxPostgresBreachRepository {
         product: &BreachProduct,
         product_version: &BreachProductVersion,
     ) -> Result<Breach, DomainError> {
-        let query = "SELECT * FROM cti.breaches WHERE id = $1";
-        let query = sqlx::query_as(query).bind(id.value());
+        let query = "SELECT * FROM cti.breaches WHERE cve_id = $1, vendor = $2, product = $3, product_version = $4";
+        let query = sqlx::query_as(query)
+            .bind(cve_id.value())
+            .bind(vendor.value())
+            .bind(product.value())
+            .bind(product_version.value());
         let key_res: Result<SqlxBreach, sqlx::Error> = query.fetch_one(&self.pool).await;
         if key_res.is_err() {
             return match key_res.err().unwrap() {
-                sqlx::Error::RowNotFound => Err(DomainError::CveNotFound { id: id.value() }),
+                sqlx::Error::RowNotFound => Err(DomainError::BreachNotFound { 
+                    cve_id: cve_id.value(),
+                    vendor: vendor.value(),
+                    product: product.value(),
+                    product_version: product_version.value(),
+                }),
                 err => {
                     error!("Error: {:?}", err);
                     Err(DomainError::Unknown)
@@ -59,25 +68,37 @@ impl BreachRepository for SqlxPostgresBreachRepository {
         Ok(key_res.unwrap().to_domain())
     }
 
-    async fn create_one(&self, cve: &Breach) -> Result<(), DomainError> {
-        let sql_cve: SqlxBreach = SqlxBreach::from_domain(cve);
+    async fn create_one(&self, breach: &Breach) -> Result<(), DomainError> {
+        let sql_breach: SqlxBreach = SqlxBreach::from_domain(breach);
         let query =
-            "INSERT INTO cti.breaches (id, state, description, assigner_id, assigner_name, date_published, date_updated) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+            "INSERT INTO cti.breaches (id, vendor, product, product_version, product_type, cve_id, cve_state,
+            cve_description, cve_assigner_id, cve_assigner_name, cve_date_published, cve_date_updated) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)";
         let res = sqlx::query(query)
-            .bind(&sql_cve.id)
-            .bind(&sql_cve.state)
-            .bind(&sql_cve.description)
-            .bind(&sql_cve.assigner_id)
-            .bind(&sql_cve.assigner_name)
-            .bind(&sql_cve.date_published)
-            .bind(&sql_cve.date_updated)
+            .bind(&sql_breach.id)
+            .bind(&sql_breach.vendor)
+            .bind(&sql_breach.product)
+            .bind(&sql_breach.product_version)
+            .bind(&sql_breach.product_type)
+            .bind(&sql_breach.cve_id)
+            .bind(&sql_breach.cve_state)
+            .bind(&sql_breach.cve_description)
+            .bind(&sql_breach.cve_assigner_id)
+            .bind(&sql_breach.cve_assigner_name)
+            .bind(&sql_breach.cve_date_published)
+            .bind(&sql_breach.cve_date_updated)
             .fetch_optional(&self.pool)
             .await;
         if res.is_err() {
             // TODO: check sql error code or message
             return match res.err().unwrap() {
                 sqlx::Error::Database(_) => {
-                    Err(DomainError::CveAlreadyExists { id: cve.id.value() })
+                    Err(DomainError::BreachNotFound { 
+                        cve_id: breach.cve_id.value(),
+                        vendor: breach.vendor.value(),
+                        product: breach.product.value(),
+                        product_version: breach.product_version.value(),
+                    })
                 }
                 err => {
                     error!("Error: {:?}", err);
@@ -88,26 +109,35 @@ impl BreachRepository for SqlxPostgresBreachRepository {
         Ok(())
     }
 
-    async fn update_one(&self, cve: &Breach) -> Result<(), DomainError> {
-        let sql_cve: SqlxBreach = SqlxBreach::from_domain(cve);
+    async fn update_one(&self, breach: &Breach) -> Result<(), DomainError> {
+        let sql_breach: SqlxBreach = SqlxBreach::from_domain(breach);
         let query =
-            "UPDATE cti.breaches SET state = $1, description = $2, assigner_id = $3, assigner_name = $4, date_published = $5, 
-            date_updated = $6 WHERE id = $7";
+            "UPDATE cti.breaches SET product_type = $1, cve_state = $2, cve_description = $3, cve_assigner_id = $4, cve_assigner_name = $5, 
+            cve_date_published = $6, cve_date_updated = $7 WHERE cve_id = $7, vendor = $8, product = $9, product_version = $10";
         let res = sqlx::query(query)
-            .bind(&sql_cve.state)
-            .bind(&sql_cve.description)
-            .bind(&sql_cve.assigner_id)
-            .bind(&sql_cve.assigner_name)
-            .bind(&sql_cve.date_published)
-            .bind(&sql_cve.date_updated)
-            .bind(&sql_cve.id)
+            .bind(&sql_breach.product_type)
+            .bind(&sql_breach.cve_state)
+            .bind(&sql_breach.cve_description)
+            .bind(&sql_breach.cve_assigner_id)
+            .bind(&sql_breach.cve_assigner_name)
+            .bind(&sql_breach.cve_date_published)
+            .bind(&sql_breach.cve_date_updated)
+            .bind(&sql_breach.cve_id)
+            .bind(&sql_breach.vendor)
+            .bind(&sql_breach.product)
+            .bind(&sql_breach.product_version)
             .fetch_optional(&self.pool)
             .await;
 
         if res.is_err() {
             // TODO: check sql error code or message
             return match res.err().unwrap() {
-                sqlx::Error::RowNotFound => Err(DomainError::CveNotFound { id: cve.id.value() }),
+                sqlx::Error::RowNotFound => Err(DomainError::BreachNotFound {
+                    cve_id: breach.cve_id.value(),
+                    vendor: breach.vendor.value(),
+                    product: breach.product.value(),
+                    product_version: breach.product_version.value(),
+                }),
                 err => {
                     error!("Error: {:?}", err);
                     Err(DomainError::Unknown)
@@ -120,19 +150,27 @@ impl BreachRepository for SqlxPostgresBreachRepository {
     async fn delete_one(
         &self,
         cve_id: &CveId,
-        vendor: &BreachVendor,
-        product: &BreachProduct,
-        product_version: &BreachProductVersion,
+        vendor: BreachVendor,
+        product: BreachProduct,
+        product_version: BreachProductVersion,
     ) -> Result<(), DomainError> {
-        let query = "DELETE FROM cti.breaches WHERE id = $1";
+        let query = "DELETE FROM cti.breaches WHERE cve_id = $1, vendor = $2, product = $3, product_version = $4";
         let res = sqlx::query(query)
-            .bind(id.value())
+            .bind(cve_id.value())
+            .bind(vendor.value())
+            .bind(product.value())
+            .bind(product_version.value())
             .fetch_optional(&self.pool)
             .await;
         if res.is_err() {
             // TODO: check sql error code or message
             return match res.err().unwrap() {
-                sqlx::Error::RowNotFound => Err(DomainError::CveNotFound { id: id.value() }),
+                sqlx::Error::RowNotFound => Err(DomainError::BreachNotFound {
+                    cve_id: cve_id.value(),
+                    vendor: vendor.value(),
+                    product: product.value(),
+                    product_version: product_version.value(),
+                }),
                 err => {
                     error!("Error: {:?}", err);
                     Err(DomainError::Unknown)
