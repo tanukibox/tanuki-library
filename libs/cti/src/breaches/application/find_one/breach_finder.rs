fce9580fd207b88eebeac0482368ebc603ebb620
Javier Parada <javierparada@pm.me>
2024-12-19 10:05:02 +0100

    opt imports

--- libs/cti/src/breaches/application/find_one/breach_finder.rs
+++ libs/cti/src/breaches/application/find_one/breach_finder.rs
@@ -1,7 +1,15 @@
 use tracing::debug;
 
 use crate::{
-    breaches::domain::{entities::{breach::Breach, breach_product::BreachProduct, breach_product_version::BreachProductVersion, breach_vendor::BreachVendor}, repositories::breach_repository::BreachRepository}, cves::domain::entities::{cve::Cve, cve_id::CveId}, shared::domain::errors::DomainError
+    breaches::domain::{
+        entities::{
+            breach::Breach, breach_product::BreachProduct,
+            breach_product_version::BreachProductVersion, breach_vendor::BreachVendor,
+        },
+        repositories::breach_repository::BreachRepository,
+    },
+    cves::domain::entities::{cve::Cve, cve_id::CveId},
+    shared::domain::errors::DomainError,
 };
 use std::sync::Arc;
 
@@ -11,9 +19,7 @@ pub struct BreachFinder<R: BreachRepository> {
 
 impl<R: BreachRepository> BreachFinder<R> {
     pub fn new(repository: Arc<R>) -> BreachFinder<R> {
-        BreachFinder {
-            repository,
-        }
+        BreachFinder { repository }
     }
 
     pub async fn run(
@@ -23,14 +29,26 @@ impl<R: BreachRepository> BreachFinder<R> {
         product_version: BreachProductVersion,
         cve_id: CveId,
     ) -> Result<Breach, DomainError> {
-        debug!("Finding Breach with CVE: {} for Product: {}:{}:{}.", cve_id, vendor, product, product_version);
-        let res = self.repository.find_one(&cve_id, &vendor, &product, &product_version).await;
+        debug!(
+            "Finding Breach with CVE: {} for Product: {}:{}:{}.",
+            cve_id, vendor, product, product_version
+        );
+        let res = self
+            .repository
+            .find_one(&cve_id, &vendor, &product, &product_version)
+            .await;
         if res.is_err() {
-            debug!("Error finding Breach with CVE: {} for Product: {}:{}:{}.", cve_id, vendor, product, product_version);
+            debug!(
+                "Error finding Breach with CVE: {} for Product: {}:{}:{}.",
+                cve_id, vendor, product, product_version
+            );
             return Err(res.err().unwrap());
         }
-        debug!("Breach with CVE: {} for Product: {}:{}:{}.", cve_id, vendor, product, product_version);
-        
+        debug!(
+            "Breach with CVE: {} for Product: {}:{}:{}.",
+            cve_id, vendor, product, product_version
+        );
+
         return res;
     }
 }
--- libs/cti/src/breaches/application/find_one/mod.rs
+++ libs/cti/src/breaches/application/find_one/mod.rs
@@ -1,2 +1,2 @@
-pub mod find_breach_query;
 pub mod breach_finder;
+pub mod find_breach_query;
